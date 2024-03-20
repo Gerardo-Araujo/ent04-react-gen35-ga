859f342265a3817032f32bddf95f59538a131d0b
GerardoAraujo <gerardo.araujos@gmail.com>
2024-03-20 15:15:16 -0500

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,58 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import useCrud from './hooks/UseCrud.js'
-import FormUser from './assets/components/FormUser'
-import UserCard from './hooks/UserCard'
+import { useEffect, useState } from "react";
+import "./App.css";
+import useCrud from "./hooks/UseCrud.js";
+import FormUser from "./assets/components/FormUser";
+import UserCard from "./hooks/UserCard";
 
-function App () {
+function App() {
+  const [userEdit, setUserEdit] = useState();
+  const [formIsClose, setformIsClose] = useState(true);
 
-  const [ userEdit, setUserEdit ] = useState()
-  const [ formIsClose, setformIsClose ] = useState(true)
+  const BASEURL = "https://users-crud.academlo.tech";
+  const [users, getUsers, createUser, deleteUser, updateUser] =
+    useCrud(BASEURL);
 
-  const BASEURL = 'https://users-crud.academlo.tech'
-const  [ users, getUsers, createUser, deleteUser, updateUser ]=  useCrud(BASEURL)
+  useEffect(() => {
+    getUsers("/users/");
+  }, []);
 
-useEffect(() => {
-  getUsers('/users/')
-
-}, [])
-
-const handleOpenForm = () => {
-  setformIsClose(false)
-}
+  const handleOpenForm = () => {
+    setformIsClose(false);
+  };
 
   return (
-    <div className='app'>
-      <header className='app__header'>
-      <h1 className='form__title'>User CRUD</h1>
-      <button onClick={handleOpenForm} className='form__btn'>Create New User</button>
+    <div className="app">
+      <header className="app__header">
+        <h1 className="form__title">User CRUD</h1>
+        <button onClick={handleOpenForm} className="form__btn">
+          Create New User
+        </button>
       </header>
-      <FormUser 
-      createUser={createUser}
-      userEdit={userEdit}
-      updateUser={updateUser}
-      setUserEdit={setUserEdit}
-      formIsClose={formIsClose}
-      setformIsClose={setformIsClose}
+      <div className="div__h2">
+       
+        <h2 className="form__title__h2">CREACION DE USUARIOS</h2>
+      </div>
+      <FormUser
+        createUser={createUser}
+        userEdit={userEdit}
+        updateUser={updateUser}
+        setUserEdit={setUserEdit}
+        formIsClose={formIsClose}
+        setformIsClose={setformIsClose}
       />
-      <div className='tempo'>
-        {
-          users?.map(user => ( 
-            <UserCard 
+      <div className="tempo">
+        {users?.map((user) => (
+          <UserCard
             key={user.id}
             user={user}
             deleteUser={deleteUser}
             setUserEdit={setUserEdit}
             handleOpenForm={handleOpenForm}
-            
-            />
-          ))
-        }
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
